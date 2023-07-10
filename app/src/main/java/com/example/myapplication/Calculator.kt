60f1f368548349faecfe57a7065f64d31ecb0e80
DemoForCD <sonalipatole8407@gmail.com>
2023-07-10 22:21:03 +0530

    Added calculator class

---
+++ app/src/main/java/com/example/myapplication/Calculator.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+class Calculator {
+    fun addition(x: Int, y: Int): Int {
+        return x + y
+    }
+
+    fun subtraction(x: Int, y: Int): Int {
+        return x - y
+    }
+
+    fun division(x: Int, y: Int): Int {
+        return x / y
+    }
+
+    fun multiply(x: Int, y: Int): Int {
+        return x * y
+    }
+
+    fun modulus(x: Int, y: Int): Int {
+        return x % y
+    }
+}
